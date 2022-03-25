1ffae2c4a1d3088c618d897713a881ae9e167537
sagor1155 <sagor.eee.cuet@gmail.com>
2022-03-25 16:36:22 +0600

    serverless yml modified

--- myServerlessProject/lambdas/main.go
+++ myServerlessProject/lambdas/main.go
@@ -7,14 +7,16 @@ import (
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-lambda-go/lambdacontext"
 )
 
 type Response events.APIGatewayProxyResponse
 type Request events.APIGatewayProxyRequest
 
 func Handler(ctx context.Context, req Request) (Response, error) {
-	fmt.Println("Path:", req.Path)
-	fmt.Println("Method:", req.HTTPMethod)
+	fmt.Println("Invoked Lambda:", lambdacontext.FunctionName)
+	fmt.Println("HTTP Path:", req.Path)
+	fmt.Println("HTTP Method:", req.HTTPMethod)
 
 	body := map[string]string{
 		"message": "Lambda executed successfully",
@@ -24,11 +26,11 @@ func Handler(ctx context.Context, req Request) (Response, error) {
 	b, _ := json.Marshal(body)
 
 	resp := Response{
-		StatusCode: 200,
-		Body:       string(b),
 		Headers: map[string]string{
 			"Content-Type": "application/json",
 		},
+		StatusCode: 200,
+		Body:       string(b),
 	}
 
 	return resp, nil
